7d42095fc20d78aac636584403af85a7c2c20fe0
Jonathan Colegrove <Demetrius472@gmail.com>
2016-04-08 16:54:59 -0600

    changed time & some text, made more DRY

--- Babysitter.rb
+++ Babysitter.rb
@@ -8,8 +8,8 @@
   def startingHour
       puts "Hi, #{@name}! When would you be starting?
             Please enter any hour from 5pm until 3am"
-      @starting = gets.chomp
-      unless @starting.delete("^0-9").to_i <5
+      @starting = gets.chomp.delete("^0-9[ ][aApP][mM]").to_i
+      unless @starting <5
           return endingHour
       else
         puts "Sorry, what was that again?"
@@ -20,36 +20,36 @@
   def endingHour
       puts "And when would you be babysitting until?
             Please enter any hour from 6pm until 4am"
-      @ending = gets.chomp
-      unless @ending.delete("^0-9").to_i >=4|| @ending.delete("^0-9").to_i>6
+      @ending = gets.chomp.delete("^0-9[ ][aApP][mM]").to_i
+      #unless @ending >=4 && @ending>6
           return bedTime
-      else
-        puts "Sorry, what was that again?"
-        return endingHour
-      end
+      #else
+      #  puts "Sorry, what was that again?"
+      #  return endingHour
+      #end
   end
 
   def bedTime
       puts "When do the children go to bed?"
-      @bedTime=gets.chomp
+      @bedTime=gets.chomp.delete("^0-9[ ][aApP][mM]").to_i
       puts "For every whole hour from:
                 5pm until bedtime (#{@bedTime}), the rate is $12/hour.
                 #{@bedTime} until midnight, the rate is $8/hr.
                 midnight until 4am, the rate is $16/hr.
-            There is no pay for parts of an hour."
+            There is no pay for incomplete hours."
             return hours
   end
 
   def hours
     case
-    when @starting.delete("^0-9").to_i >=5 && @ending.delete("^0-9").to_i <= @bedTime.delete("^0-9").to_i
-    puts "Your pay is $#{(@ending.to_i-@starting.to_i.to_i)*12}."
+    when @starting >=5 && @ending <= @bedTime
+    puts "Your pay is $#{(@ending-@starting)*12}."
 
-    when @starting.delete("^0-9").to_i >=@bedTime.delete("^0-9").to_i && @ending.delete("^0-9").to_i <= 12
-    puts "Your pay is $#{(@ending.to_i-@starting.to_i)*8}."
+    when @starting >=@bedTime && @ending <= 12
+    puts "Your pay is $#{(@ending-@starting)*8}."
 
-    when @starting.delete("^0-9").to_i >=12 && @ending.delete("^0-9").to_i <= 4
-    puts "Your pay is $#{(@ending.to_i-@starting.to_i)*16}."
+    when @starting >=12 && @ending <= 4
+    puts "Your pay is $#{(@ending-@starting)*16}."
     end
   end
 
