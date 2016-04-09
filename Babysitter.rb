301e58303becf0c9de4aa937270a31baa7dd7d72
Jonathan Colegrove <Demetrius472@gmail.com>
2016-04-08 23:00:24 -0600

    improved pay system

--- Babysitter.rb
+++ Babysitter.rb
@@ -9,7 +9,10 @@
       puts "Hi, #{@name}! When would you be starting?
             Please enter any hour from 5pm until 3am"
       @starting = gets.chomp.delete("^0-9[ ][aApP][mM]").to_i
-      unless @starting <5
+          if @starting<17
+            then @starting+=12
+          end
+      unless @starting<17
           return endingHour
       else
         puts "Sorry, what was that again?"
@@ -21,37 +24,56 @@
       puts "And when would you be babysitting until?
             Please enter any hour from 6pm until 4am"
       @ending = gets.chomp.delete("^0-9[ ][aApP][mM]").to_i
-      #unless @ending >=4 && @ending>6
+      if @ending<12
+        then @ending+=12
+      end
+      #unless @ending>=18 && @ending<=28
           return bedTime
       #else
-      #  puts "Sorry, what was that again?"
+       # puts "Sorry, what was that again?"
       #  return endingHour
-      #end
+     # end
   end
 
   def bedTime
       puts "When do the children go to bed?"
       @bedTime=gets.chomp.delete("^0-9[ ][aApP][mM]").to_i
+      if @bedTime<12
+        then @bedTime+=12
+      end
       puts "For every whole hour from:
                 5pm until bedtime (#{@bedTime}), the rate is $12/hour.
                 #{@bedTime} until midnight, the rate is $8/hr.
                 midnight until 4am, the rate is $16/hr.
             There is no pay for incomplete hours."
-            return hours
+  #    return hours
+  return finalPay
   end
 
-  def hours
-    case
-    when @starting >=5 && @ending <= @bedTime
-    puts "Your pay is $#{(@ending-@starting)*12}."
 
-    when @starting >=@bedTime && @ending <= 12
-    puts "Your pay is $#{(@ending-@starting)*8}."
+def finalPay
+  $payBeforeBedtime = (@starting-@ending)*12
+  $payAfterBedtime = (24-@bedTime)*8
+  #28= 4am (24hr day + 4hrs)
+  $payAfterMidnight = (28-@ending)*16
 
-    when @starting >=12 && @ending <= 4
-    puts "Your pay is $#{(@ending-@starting)*16}."
-    end
-  end
+  @finalPay = $payBeforeBedtime+$payAfterBedtime+$payAfterMidnight
+  puts "Your will earn $#{@finalPay} for babysitting today!"
+end
+
+
+ # def hours
+ #   case
+ #   when @starting>=17 && @ending<=@bedTime
+ #   puts "Your pay is $#{(@ending-@starting)*12}."
+
+ #   when @starting>=@bedTime && @ending<=12
+ #   puts "Your pay is $#{(@ending-@starting)*8}."
+
+ #   when @starting>=24 && @ending<=4
+ #   puts "Your pay is $#{(@ending-@starting)*16}."
+ #   end
+ # end
 
 
 welcome
