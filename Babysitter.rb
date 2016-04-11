73bc7eeb6ff7faf37e605361fe1c243864d28781
Jonathan Colegrove <Demetrius472@gmail.com>
2016-04-11 13:06:31 -0600

    Fixed math values. Idk what I was thinking before

--- Babysitter.rb
+++ Babysitter.rb
@@ -41,39 +41,27 @@
       if @bedTime<12
         then @bedTime+=12
       end
-      puts "For every whole hour from:
-                5pm until bedtime (#{@bedTime-12}), the rate is $12/hour.
-                #{@bedTime-12} until midnight, the rate is $8/hr.
-                midnight until 4am, the rate is $16/hr.
-            There is no pay for incomplete hours."
-  #    return hours
   return finalPay
   end
 
 
 def finalPay
-  $payBeforeBedtime = (@ending-@starting)*12
-  $payAfterBedtime = (24-@bedTime)*8
-  #28= 4am (24hr day + 4hrs)
-  $payAfterMidnight = (28-@ending)*16
+    puts "For every whole hour from:
+              5pm until bedtime (#{@bedTime-12}), the rate is $12/hour.
+              #{@bedTime-12} until midnight, the rate is $8/hr.
+              midnight until 4am, the rate is $16/hr.
+          There is no pay for incomplete hours."
+  $payBeforeBedtime = (@bedTime-@starting)*12
+  $payAfterBedtime = @ending<=24 ? (@ending-@bedTime)*8 : (24-@bedTime)*8
+  $payAfterMidnight = (@ending-24)*16
 
-  @finalPay = $payBeforeBedtime+$payAfterBedtime+$payAfterMidnight
+    if @ending<24 && @ending<@bedTime
+        then @finalPay=$payBeforeBedtime
+    elsif @ending<=24 && @ending>@bedTime
+        then @finalPay=$payBeforeBedtime+$payAfterBedtime
+    else @finalPay = $payBeforeBedtime+$payAfterBedtime+$payAfterMidnight
+    end
   puts "Your will earn $#{@finalPay} for babysitting today!"
 end
-
-
- # def hours
- #   case
- #   when @starting>=17 && @ending<=@bedTime
- #   puts "Your pay is $#{(@ending-@starting)*12}."
-
- #   when @starting>=@bedTime && @ending<=12
- #   puts "Your pay is $#{(@ending-@starting)*8}."
-
- #   when @starting>=24 && @ending<=4
- #   puts "Your pay is $#{(@ending-@starting)*16}."
- #   end
- # end
-
 
 welcome
